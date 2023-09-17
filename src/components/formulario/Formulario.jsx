0e924f1ebc3e8c2af699f24ac477a409d4b3485a
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2023-09-17 19:32:33 +0200

    agregando funcionalidad

--- src/components/formulario/Formulario.jsx
+++ src/components/formulario/Formulario.jsx
@@ -1,19 +1,46 @@
-import {React, useState } from 'react'
+import { React, useState } from 'react'
 
 const Formulario = () => {
-const [name, setName] = useState("")
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
 
-const handleChange = (event) =>{ 
-  setName(event.target.value)
-};
+  const [user, setUser] = useState({})
 
-console.log(name);
+  // const handleNameChange = (event) => setName(event.target.value);
+  // const handleEmailChange = (event) => setEmail(event.target.value);
+  // const handleAgeChange = (event) => setAge(event.target.value);
+
+  const handleInput = (event) => {
+
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // const user = {
+    //   name: name,
+    //   email: email,
+    //   age: age
+    // }
+
+    console.log(user);
+    //console.log(name, email, age);
+  };
+
+
   return (
-    <form >
-        <label htmlFor="">Enter your name:</label>
-        <input type="text" name='name' value={name} onChange={handleChange} />
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Enter your name:</label>
+      <input type="text" name='name' value={name} onChange={handleInput} />
 
-        <input type="submit" />
+      <label htmlFor="email">Email:</label>
+      <input type="email" name='email' value={email} onChange={handleInput} />
+
+      <label htmlFor="age">Edad:</label>
+      <input type="text" name='age' value={age} onChange={handleInput} />
+
+
+      <input type="submit" />
     </form>
   )
 }
